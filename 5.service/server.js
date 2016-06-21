b249d81aaf08030ac466dc1846d3be5d3e7cbfff
pengyanxue <865560848@qq.com>
2016-06-21 18:14:53 +0800

    定义服务组件

---
+++ 5.service/server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/6/21.
+ */
+var http=require('http');
+var count=0;
+http.createServer(function(req,res){
+        if(count%2==0){
+
+        }
+    }).listen(8080);
