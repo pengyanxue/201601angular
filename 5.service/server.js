bb019191f9c0ea4da4d21a11abb5d0e10829791a
pengyanxue <865560848@qq.com>
2016-06-22 17:54:20 +0800

    jsonp  拦截器

--- 5.service/server.js
+++ 5.service/server.js
@@ -1,10 +1,12 @@
-/**
- * Created by Administrator on 2016/6/21.
- */
-var http=require('http');
-var count=0;
-http.createServer(function(req,res){
-        if(count%2==0){
-
-        }
-    }).listen(8080);
+var http = require('http');
+var count = 0;
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (count++ % 2 == 0) {
+        res.statusCode = 500;
+        res.end('500');
+    } else {
+        res.statusCode = 200;
+        res.end('200')
+    }
+}).listen(8080);
