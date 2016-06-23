7913908ffbbefa96f6cd5a66a27e475eb214a313
pengyanxue <865560848@qq.com>
2016-06-23 13:48:22 +0800

    ngroute filter

--- 5.service/resourceserver.js
+++ 5.service/resourceserver.js
@@ -2,19 +2,41 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static(__dirname));
-app.use(require('body-parser'),json());
+app.use(require('body-parser').json());
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve('naresource.html'));
+    res.sendFile(path.resolve('ngresource.html'));
 });
-var books=[{id:1,name:'node.js'}];
+var books = [{id: 1, name: 'node.js'}];
+
+
+
+
 //当用户请求/books路径的时候
-app.route('/books').get(function(req,res){
-    res.send();
-}).post((req,res)=>{//增加一个资源
+app.route('/books').get(function (req, res) {
+    res.send(books);
+}).post((req, res)=> {//增加一个资源
     //如何接收客户端传过来的post请求体
-    var book=req.body;
-    book.id=books[books.length-1]+1;
-    books.push({book});
+    var book = req.body;
+    book.id = books[books.length - 1].id + 1;
+    books.push(book);
     res.send(book);
 });
-app.listen(8080);
+app.route('/books/:id').delete((req, res)=> {
+    books = books.filter((book)=> {
+        return book.id != req.params.id;
+
+    });
+    res.send({});
+}).put((req, res)=> {
+    books = books.map(item => {
+        if (item.id == req.params.id) {
+            return req.body;
+        } else {
+            return item;
+        }
+    });
+    res.send(req.body);
+}).get((req,res)=>{
+    res.send(books[0]);
+});
+app.listen(9090);
